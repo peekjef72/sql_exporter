195f2d9abd157356a7628639b5cb0229ca1b31ed
peekjef72 <jfpik78@gmail.com>
2022-06-18 08:08:03 +0200

    add a target dry-mode: collect one target and exit

--- cmd/sql_exporter/main.go
+++ cmd/sql_exporter/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"os"
 	"runtime"
+	"time"
 
 	_ "net/http/pprof"
 
@@ -13,6 +15,7 @@ import (
 	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/expfmt"
 	"github.com/prometheus/common/promlog"
 	"github.com/prometheus/common/promlog/flag"
 	"github.com/prometheus/common/version"
@@ -30,7 +33,8 @@ var (
 	metricsPath   = kingpin.Flag("web.telemetry-path", "Path under which to expose collector's internal metrics.").Default("/metrics").String()
 	configFile    = kingpin.Flag("config.file", "MSSQL Exporter configuration file.").Short('c').Default("config/config.yml").String()
 	debug_flag    = kingpin.Flag("debug", "debug connection checks.").Short('d').Default("false").Bool()
-	dry_run       = kingpin.Flag("dry-run", "Only check exporter configuration file and exit.").Short('n').Default("false").Bool()
+	dry_run       = kingpin.Flag("dry-run", "check exporter configuration file and try to collect a target then exit.").Short('n').Default("false").Bool()
+	target_name   = kingpin.Flag("target", "In dry-run mode specify the target name, else ignored.").Short('t').String()
 	// alsologtostderr = kingpin.Flag("alsologtostderr", "log to standard error as well as files.").Default("true").Bool()
 )
 
@@ -80,6 +84,60 @@ func main() {
 
 	if *dry_run {
 		level.Info(logger).Log("msg", "configuration OK.")
+		// get the target if defined
+		var t sql_exporter.Target
+		var err error
+		if *target_name != "" {
+			t, err = exporter.FindTarget(*target_name)
+		} else {
+			t, err = exporter.GetFirstTarget()
+		}
+		if err != nil {
+			level.Error(logger).Log("errmsg", err)
+			os.Exit(1)
+		}
+		level.Info(logger).Log("msg", fmt.Sprintf("try to collect target %s.", t.Name()))
+		timeout := time.Duration(0)
+		configTimeout := time.Duration(exporter.Config().Globals.ScrapeTimeout)
+
+		// If the configured scrape timeout is more restrictive, use that instead.
+		if configTimeout > 0 && (timeout <= 0 || configTimeout < timeout) {
+			timeout = configTimeout
+		}
+		var ctx context.Context
+		var cancel context.CancelFunc
+		if timeout <= 0 {
+			ctx = context.Background()
+			cancel = func() {}
+		} else {
+			ctx, cancel = context.WithTimeout(context.Background(), timeout)
+		}
+		defer cancel()
+
+		gatherer := prometheus.Gatherers{exporter.WithContext(ctx, t)}
+		mfs, err := gatherer.Gather()
+		if err != nil {
+			level.Error(logger).Log("errmsg", fmt.Sprintf("Error gathering metrics: %v", err))
+			os.Exit(1)
+		}
+
+		level.Info(logger).Log("msg", "collect is OK. Dumping result to stdout.")
+
+		//dump metric to stdout
+		enc := expfmt.NewEncoder(os.Stdout, expfmt.FmtText)
+
+		for _, mf := range mfs {
+			err := enc.Encode(mf)
+			if err != nil {
+				level.Error(logger).Log("Errmsg", err)
+				break
+			}
+		}
+		if closer, ok := enc.(expfmt.Closer); ok {
+			// This in particular takes care of the final "# EOF\n" line for OpenMetrics.
+			closer.Close()
+		}
+		level.Info(logger).Log("msg", "dry-run is over. Exiting.")
 		os.Exit(0)
 	}
 
